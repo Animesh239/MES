cf4c5b60225ccadcf44a6acfe7711f5fb667f71e
AYANscyy2 <mayan6378@gmail.com.com>
2024-12-10 22:31:48 +0530

    feat:update homepage route

--- mining-engineering-society/src/app/(routes)/page.tsx
+++ mining-engineering-society/src/app/(routes)/page.tsx
@@ -1,16 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { HomePageMain } from "@/components/features/HomePage/Body/HomePageMain";
 
 export default function Home() {
   return (
     <>
-      <div className=" bg-[#000022] w-screen h-screen flex justify-center items-center">
-        <Link href="/homepage">
-          <Button variant="destructive" size="default">
-            HomePage
-          </Button>
-        </Link>
-      </div>
+      <HomePageMain />
     </>
   );
 }
