2780db44dd9290e57ecd69c3bf1500412c87ed17
AYANscyy2 <mayan6378@gmail.com>
2025-01-30 04:49:50 +0530

    feat:initialise minare landing page

---
+++ mining-engineering-society/src/components/features/Minare/landing/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "./HeroSection/heroSection";
+
+export const LandingPage = () => {
+  return (
+    <div className="min-h-screen h-auto star-container p-[16px]">
+      <Hero />
+    </div>
+  );
+};
--- mining-engineering-society/tailwind.config.ts
+++ mining-engineering-society/tailwind.config.ts
@@ -5,63 +5,67 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "metallic-gradient":
+          "linear-gradient(to bottom, #ffffff 0%, #d4d4d4 50%, #a3a3a3 100%)"
+      },
       animation: {
-        "rainbow-shadow": "rainbowShadow 5s linear infinite",
+        "rainbow-shadow": "rainbowShadow 5s linear infinite"
       },
       keyframes: {
         rainbowShadow: {
           "0%": { boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #f00" },
           "20%": {
-            boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #ff0",
+            boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #ff0"
           },
           "40%": {
-            boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #0f0",
+            boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #0f0"
           },
           "60%": {
-            boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #0ff",
+            boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #0ff"
           },
           "80%": {
-            boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #00f",
+            boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #00f"
           },
           "100%": {
-            boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #f0f",
-          },
-        },
+            boxShadow: "0 0 2px #fff, inset 0 0 2px #fff, 0 0 5px #f0f"
+          }
+        }
       },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         card: {
           DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          foreground: "hsl(var(--card-foreground))"
         },
         popover: {
           DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          foreground: "hsl(var(--popover-foreground))"
         },
         primary: {
           DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          foreground: "hsl(var(--primary-foreground))"
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          foreground: "hsl(var(--secondary-foreground))"
         },
         muted: {
           DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          foreground: "hsl(var(--muted-foreground))"
         },
         accent: {
           DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          foreground: "hsl(var(--accent-foreground))"
         },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          foreground: "hsl(var(--destructive-foreground))"
         },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -71,19 +75,24 @@ export default {
           "2": "hsl(var(--chart-2))",
           "3": "hsl(var(--chart-3))",
           "4": "hsl(var(--chart-4))",
-          "5": "hsl(var(--chart-5))",
-        },
+          "5": "hsl(var(--chart-5))"
+        }
       },
       screens: {
         xsm: "475px",
-        xxsm: "390px",
+        xxsm: "390px"
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        sm: "calc(var(--radius) - 4px)"
       },
-    },
+      fontFamily: {
+        Bebas: "var(--font-Bebas_Neue)",
+        orbitron: "var(--font-Orbitron)",
+        rajdhani: "var(--font-Rajdhani)"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
