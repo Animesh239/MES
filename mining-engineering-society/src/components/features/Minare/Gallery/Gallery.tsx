6b09770b203aa021443bc0026484e8af950850ae
surajitnk <surajitangul@gmail.com>
2025-03-05 00:36:33 +0530

    added minare images

--- mining-engineering-society/src/components/features/Minare/Gallery/Gallery.tsx
+++ mining-engineering-society/src/components/features/Minare/Gallery/Gallery.tsx
@@ -1,45 +1,23 @@
 "use client";
-import { useState, useEffect } from "react";
 import Image from "next/image";
 import { galleryData } from "@/config/Minare/Gallery/Data";
 import { Header } from "../landing/header";
 
-interface ImageDimensions {
-  width: number;
-  height: number;
-}
-
 export default function Gallery() {
-  const [imageDimensions, setImageDimensions] = useState<ImageDimensions[]>([]);
-
-  useEffect(() => {
-    const dimensions = galleryData.images.map(() => ({
-      width: Math.floor(Math.random() * (600 - 300 + 1)) + 300,
-      height: Math.floor(Math.random() * (400 - 200 + 1)) + 200,
-    }));
-    setImageDimensions(dimensions);
-  }, []);
-
   return (
     <div className="min-h-screen bg-black text-white p-8 mb-12">
       <div className="mt-20 mb-12">
         <Header label={galleryData.title} />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
-        {galleryData.images.map((image, index) => (
+        {galleryData.images.map((image) => (
           <div
             key={image.id}
             className="relative overflow-hidden rounded-lg shadow-lg"
-            style={{
-              paddingBottom: `${
-                (imageDimensions[index]?.height /
-                  imageDimensions[index]?.width) *
-                100
-              }%`,
-            }}
+            style={{ paddingBottom: `${(image.height / image.width) * 100}%` }}
           >
             <Image
-              src={`https://picsum.photos/${imageDimensions[index]?.width}/${imageDimensions[index]?.height}?random=${image.id}`}
+              src={image.src}
               alt={image.alt}
               layout="fill"
               objectFit="cover"
